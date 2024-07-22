422b0e0c58861b2780e1d974b1913a5db3092f9a
Saarini Ritesh <saarini.ritesh@gmail.com>
2024-07-22 22:48:30 +0530

    27

---
+++ 27.py
@@ -0,0 +1,13 @@
+#27. Remove Element
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        for j in nums:
+            if j != val:
+                nums[i] = j
+                i += 1
+        return i
+
+
+        
